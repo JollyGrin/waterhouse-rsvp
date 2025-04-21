c25aa0a2c487c7c5d64ae06277df05bc2f78dbee
JollyGrin <lol@dean.lol>
2025-04-21 18:19:53 +0200

    change env import

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+	import env from '$env/static/public';
 	import '../app.css';
 	import { ClerkProvider } from 'svelte-clerk';
 
 	let { children } = $props();
 </script>
 
-<ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+<ClerkProvider publishableKey={env?.PUBLIC_CLERK_PUBLISHABLE_KEY}>
 	{@render children()}
 </ClerkProvider>
